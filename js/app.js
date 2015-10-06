7ded9b5747741926d4d1b895165a6e35cea2a80d
RizOrt218 <rizhiafalen@gmail.com>
2015-10-06 03:10:55 -1000

    created divs and inserted cart image whooHooo

--- js/app.js
+++ js/app.js
@@ -1,43 +1,49 @@
 window.onload = function(){
 	var arrayItems = [
-  { "name": "Lusicious Jello Mix", "description": ["Very Elegant", "Trending item", "Come in Purple"], "price": 80.65 },
-  { "name": "Tarnished Standing Desk", "description": ["Modular", "Works for both Tall and Loud People", "Smells like Productivity"], "price": 1654.99},
-  { "name": "Hand-made Hand Grenades", "description": ["Such gift!", "Much boom!", "Very safe for kids"], "price": 10.44},
-  { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", "Family-size", "Hot Mess"], "price": 16.99 },
-  { "name": "Fancy Dress Hanger", "description": ["Keep organized", "On Sale"], "price": 67.32 },
-  { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", "Hugs not drugs", "As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
+  { "name": "Lusicious Jello Mix", "description": ["Very Elegant", " Trending item", " Come in Purple"], "price": 80.65 },
+  { "name": "Tarnished Standing Desk", "description": ["Modular", " Works for both Tall and Loud People", " Smells like Productivity "], "price": 1654.99},
+  { "name": "Hand-made Hand Grenades", "description": ["Such gift!", " Much boom!", " Very safe for kids"], "price": 10.44},
+  { "name": "Pan-fried Cookie Dough", "description": ["Chocolate", " Family-size", " Hot Mess"], "price": 16.99 },
+  { "name": "Fancy Dress Hanger", "description": ["Keep organized", " On Sale"], "price": 67.32 },
+  { "name": "Snarky Britsh Mustache 3-Pack", "description": ["Sharing is caring!", " Hugs not drugs", " As seen on 'So You Think You Can Dance - Nigeria!'"], "price": 1.99 },
 ];
   
  
 
   function getObject(arrs){
 
-    // var divBody = document.createElement('div');
-    // divBody.id = 'divName';
-    // document.body.appendChild(divBody);
-
-    // var parBody = document.createElement('p');
-    // parBody.id = 'parDes';
-    // document.body.appendChild(parBody);
-
     var cart = document.getElementById('container');
 
-    for(var i=0; i<arrs.length; i++){ //loop through arrayItems
+
+
+    for(var i=0; i<arrs.length; i++){                    //loop through arrayItems
+
+      //created each array a div
+      var cartDiv = document.createElement('div');
+      cartDiv.id = 'arrayDiv';
+      cart.appendChild(cartDiv);
+
+      //created element for object 'name'
       console.log(arrs[i].name);
       var itemName = document.createElement('h3');
       itemName.id = 'headName';
       itemName.innerHTML = arrs[i].name;
-      cart.appendChild(itemName);
+      cartDiv.appendChild(itemName);
 
+      //created element for object 'description'
       console.log(arrs[i].description);
       var itemDes = document.createElement('p');
+      itemDes.id = 'itemDescription';
       itemDes.innerHTML = arrs[i].description;
-      cart.appendChild(itemDes);
+      cartDiv.appendChild(itemDes);
 
+      //created element for object 'price'
       console.log(arrs[i].price);
       var itemPrice = document.createElement('p');
+      itemPrice.id = 'priceTag';
       itemPrice.innerHTML = arrs[i].price;
-      cart.appendChild(itemPrice);
+      cartDiv.appendChild(itemPrice);
+
     }
   }
   getObject(arrayItems);
